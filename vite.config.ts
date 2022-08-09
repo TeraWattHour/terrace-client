0423d71d9f1120f2a14953964f055eb50dbdaa3e
TeraWattHour <norman.polska@gmail.com>
2022-08-09 22:23:44 +0200

    changes: dynamic port support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: process.env.PORT || 1234,
+//   },
+// });
+
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: process.env.PORT,
+    },
+    preview: {
+      port: process.env.PORT,
+    },
+  });
+};
